f3016829157f5dbf6889e319c21702113802ecd1
Hammad <hmdshfq@outlook.com>
2023-11-22 21:02:54 +0100

    feat: create first module and use it

--- simpleHTTPServer.js
+++ simpleHTTPServer.js
@@ -1,9 +1,10 @@
 const http = require('http');
+const dt = require('./dateTime');
 const PORT = 4444;
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(`Welcome to Simple HTTP Server\nYou are currently on the ${req.url} page.`);
+    res.write(`Welcome to Simple HTTP Server.\nYou are currently on the ${req.url} page.\n${dt.dateTime()}`);
     res.end();
 }).listen(PORT, () =>
     console.log(`Server running on http://localhost:${PORT}`)
